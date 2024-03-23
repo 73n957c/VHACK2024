e9b74b9a910013fa90bf8f48ff3010e750f15221
Chin Zheng Yu <22004883@siswa.um.edu.my>
2024-03-23 23:45:14 +0800

    Finished community page and topic page

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NavBar from "./components/navBar";
+import NavBar from "./components/navbar";
 import HomePage from "./pages/HomePage";
 import EducationPage from "./pages/EducationPage";
 import ChallengePage from "./pages/ChallengePage";
@@ -14,6 +14,7 @@ import CommunityPage from "./pages/CommunityPage";
 import NewsPage from "./pages/NewsPage";
 import GoalPage from "./pages/GoalPage";
 import AboutPage from "./pages/AboutPage";
+import TopicPage from "./pages/TopicPage";
 import BeginnerLevelPage from "./pages/BeginnerLevelPage";
 
 function App() {
@@ -29,6 +30,7 @@ function App() {
         <Route path="/about" element={<AboutPage />} />
         <Route path="/challenge/beginner" element={<BeginnerLevelPage />} />
         <Route path="/news" element={<NewsPage />} />
+        <Route path="/community/topic1" element={<TopicPage />} />
       </Routes>
     </Router>
   );
--- src/pages/CommunityPage.jsx
+++ src/pages/CommunityPage.jsx
@@ -1,43 +1,103 @@
-import Nav from 'react-bootstrap/Nav';
-import React, {useState} from 'react';
-import Button from 'react-bootstrap/Button';
-import CommunityTrending from '../components/communityTrending';
-import CommunityArchived from '../components/communityArchived';
-import CommunitySuggestion from '../components/communitySuggestion';
+import Nav from "react-bootstrap/Nav";
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import CommunityTrending from "../components/communityTrending";
+import CommunityArchived from "../components/communityArchived";
+import CommunitySuggestion from "../components/communitySuggestion";
+import { Container, Col, Row } from "react-bootstrap";
 import "../page-css/community.css";
 
-function Community(){
-    const [activeMenuItem, setActiveMenuItem] = useState(null);
-    const handleMenuItemClick = (index) => {
-        setActiveMenuItem(index);
-    };
+function Community() {
+  const [activeMenuItem, setActiveMenuItem] = useState(null);
+  const handleMenuItemClick = (index) => {
+    setActiveMenuItem(index);
+  };
 
-    return(
-        <div id="community">
-            <h1>Community</h1>
-            <Nav class="horizontal-nav" role="navigation">
-                <ul>
-                    <li><a><Nav.Link href="#all" style={{textDecoration: activeMenuItem === 0 ? 'underline' : 'none', color: activeMenuItem === 0 ? '#FFA800' : 'white'}} onClick={() => handleMenuItemClick(0)}>All</Nav.Link></a></li>
-                    <li><a><Nav.Link href="#general" style={{textDecoration: activeMenuItem === 1 ? 'underline' : 'none', color: activeMenuItem === 1 ? '#FFA800' : 'white'}} onClick={() => handleMenuItemClick(1)}>General</Nav.Link></a></li>
-                    <li><a><Nav.Link href="#tradequest" style={{textDecoration: activeMenuItem === 2 ? 'underline' : 'none', color: activeMenuItem === 2 ? '#FFA800' : 'white'}} onClick={() => handleMenuItemClick(2)}>TradeQuest</Nav.Link></a></li>
-                    <li><a><Nav.Link href="#weeklychallenge" style={{textDecoration: activeMenuItem === 3 ? 'underline' : 'none', color: activeMenuItem === 3 ? '#FFA800' : 'white'}} onClick={() => handleMenuItemClick(3)}>Weekly Challenge</Nav.Link></a></li>
-                </ul>
-            </Nav>
-                <div id="communitySearch">
-                    <img src="Resources/searchGroup.png" alt="Search Group" className="search-group-img" />
-                    <Button>New Topic</Button>
-                </div>
-            <div id="communityComponent">
-                <div id="communityComponentTop">
-                    <CommunityTrending></CommunityTrending>
-                    <p1><CommunityArchived></CommunityArchived></p1>
-                </div>
-                <div id="communityComponenBtm">
-                    <CommunitySuggestion></CommunitySuggestion>
-                </div>
-            </div>
+  return (
+    <Container id="community">
+      <h1>Community</h1>
+      <Nav class="horizontal-nav" role="navigation">
+        <ul>
+          <li>
+            <a>
+              <Nav.Link
+                href="#all"
+                style={{
+                  textDecoration: activeMenuItem === 0 ? "underline" : "none",
+                  color: activeMenuItem === 0 ? "#FFA800" : "white",
+                }}
+                onClick={() => handleMenuItemClick(0)}
+              >
+                All
+              </Nav.Link>
+            </a>
+          </li>
+          <li>
+            <a>
+              <Nav.Link
+                href="#general"
+                style={{
+                  textDecoration: activeMenuItem === 1 ? "underline" : "none",
+                  color: activeMenuItem === 1 ? "#FFA800" : "white",
+                }}
+                onClick={() => handleMenuItemClick(1)}
+              >
+                General
+              </Nav.Link>
+            </a>
+          </li>
+          <li>
+            <a>
+              <Nav.Link
+                href="#tradequest"
+                style={{
+                  textDecoration: activeMenuItem === 2 ? "underline" : "none",
+                  color: activeMenuItem === 2 ? "#FFA800" : "white",
+                }}
+                onClick={() => handleMenuItemClick(2)}
+              >
+                TradeQuest
+              </Nav.Link>
+            </a>
+          </li>
+          <li>
+            <a>
+              <Nav.Link
+                href="#weeklychallenge"
+                style={{
+                  textDecoration: activeMenuItem === 3 ? "underline" : "none",
+                  color: activeMenuItem === 3 ? "#FFA800" : "white",
+                }}
+                onClick={() => handleMenuItemClick(3)}
+              >
+                Weekly Challenge
+              </Nav.Link>
+            </a>
+          </li>
+        </ul>
+        <div id="communitySearch">
+          <img
+            src="Resources/searchGroup.png"
+            alt="Search Group"
+            className="search-group-img"
+          />
+          <Button>NEW TOPIC</Button>
         </div>
-    )
+      </Nav>
+
+      <Row id="communityComponent">
+        <Col sm={7} id="communityComponentTop">
+          <CommunityTrending></CommunityTrending>
+        </Col>
+        <Col sm={5}>
+          <CommunityArchived></CommunityArchived>
+        </Col>
+      </Row>
+      <div id="communityComponenBtm">
+        <CommunitySuggestion></CommunitySuggestion>
+      </div>
+    </Container>
+  );
 }
 
 export default Community;
