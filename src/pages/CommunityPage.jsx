160258d16380aa1ba59506787dbbab7072d96e0d
Chin Zheng Yu <22004883@siswa.um.edu.my>
2024-03-24 10:13:29 +0800

    changed community header spacing

--- src/pages/CommunityPage.jsx
+++ src/pages/CommunityPage.jsx
@@ -14,89 +14,93 @@ function Community() {
   };
 
   return (
-    <Container id="community">
-      <h1 className="page-title">Community</h1>
-      <Nav className="horizontal-nav" role="navigation">
-        <ul>
-          <li>
-            <a>
-              <Nav.Link
-                href="#all"
-                style={{
-                  textDecoration: activeMenuItem === 0 ? "underline" : "none",
-                  color: activeMenuItem === 0 ? "#FFA800" : "white",
-                }}
-                onClick={() => handleMenuItemClick(0)}
-              >
-                All
-              </Nav.Link>
-            </a>
-          </li>
-          <li>
-            <a>
-              <Nav.Link
-                href="#general"
-                style={{
-                  textDecoration: activeMenuItem === 1 ? "underline" : "none",
-                  color: activeMenuItem === 1 ? "#FFA800" : "white",
-                }}
-                onClick={() => handleMenuItemClick(1)}
-              >
-                General
-              </Nav.Link>
-            </a>
-          </li>
-          <li>
-            <a>
-              <Nav.Link
-                href="#tradequest"
-                style={{
-                  textDecoration: activeMenuItem === 2 ? "underline" : "none",
-                  color: activeMenuItem === 2 ? "#FFA800" : "white",
-                }}
-                onClick={() => handleMenuItemClick(2)}
-              >
-                TradeQuest
-              </Nav.Link>
-            </a>
-          </li>
-          <li>
-            <a>
-              <Nav.Link
-                href="#weeklychallenge"
-                style={{
-                  textDecoration: activeMenuItem === 3 ? "underline" : "none",
-                  color: activeMenuItem === 3 ? "#FFA800" : "white",
-                }}
-                onClick={() => handleMenuItemClick(3)}
-              >
-                Weekly Challenge
-              </Nav.Link>
-            </a>
-          </li>
-        </ul>
-        <div id="communitySearch">
-          <img
-            src="Resources/searchGroup.png"
-            alt="Search Group"
-            className="search-group-img"
-          />
-          <Button>NEW TOPIC</Button>
-        </div>
-      </Nav>
-
-      <Row id="communityComponent">
-        <Col sm={6} id="communityComponentTop">
-          <CommunityTrending></CommunityTrending>
-        </Col>
-        <Col sm={5}>
-          <CommunityArchived></CommunityArchived>
-        </Col>
-      </Row>
-      <div id="communityComponenBtm">
-        <CommunitySuggestion></CommunitySuggestion>
+    <>
+      <div className="page-title">
+        <h1>Community</h1>
       </div>
-    </Container>
+      <Container id="community">
+        <Nav className="horizontal-nav" role="navigation">
+          <ul>
+            <li>
+              <a>
+                <Nav.Link
+                  href="#all"
+                  style={{
+                    textDecoration: activeMenuItem === 0 ? "underline" : "none",
+                    color: activeMenuItem === 0 ? "#FFA800" : "white",
+                  }}
+                  onClick={() => handleMenuItemClick(0)}
+                >
+                  All
+                </Nav.Link>
+              </a>
+            </li>
+            <li>
+              <a>
+                <Nav.Link
+                  href="#general"
+                  style={{
+                    textDecoration: activeMenuItem === 1 ? "underline" : "none",
+                    color: activeMenuItem === 1 ? "#FFA800" : "white",
+                  }}
+                  onClick={() => handleMenuItemClick(1)}
+                >
+                  General
+                </Nav.Link>
+              </a>
+            </li>
+            <li>
+              <a>
+                <Nav.Link
+                  href="#tradequest"
+                  style={{
+                    textDecoration: activeMenuItem === 2 ? "underline" : "none",
+                    color: activeMenuItem === 2 ? "#FFA800" : "white",
+                  }}
+                  onClick={() => handleMenuItemClick(2)}
+                >
+                  TradeQuest
+                </Nav.Link>
+              </a>
+            </li>
+            <li>
+              <a>
+                <Nav.Link
+                  href="#weeklychallenge"
+                  style={{
+                    textDecoration: activeMenuItem === 3 ? "underline" : "none",
+                    color: activeMenuItem === 3 ? "#FFA800" : "white",
+                  }}
+                  onClick={() => handleMenuItemClick(3)}
+                >
+                  Weekly Challenge
+                </Nav.Link>
+              </a>
+            </li>
+          </ul>
+          <div id="communitySearch">
+            <img
+              src="Resources/searchGroup.png"
+              alt="Search Group"
+              className="search-group-img"
+            />
+            <Button>NEW TOPIC</Button>
+          </div>
+        </Nav>
+
+        <Row id="communityComponent">
+          <Col sm={6} id="communityComponentTop">
+            <CommunityTrending></CommunityTrending>
+          </Col>
+          <Col sm={5}>
+            <CommunityArchived></CommunityArchived>
+          </Col>
+        </Row>
+        <div id="communityComponenBtm">
+          <CommunitySuggestion></CommunitySuggestion>
+        </div>
+      </Container>
+    </>
   );
 }
 
